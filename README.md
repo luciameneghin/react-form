572e2194ab4992f1156cd86e852b1396f0ca77cd
luciameneghin <lucia.meneghin00@gmail.com>
2025-01-17 17:48:56 +0100

    add: base layout

---
+++ README.md
@@ -0,0 +1,5 @@
+# Consegna
+Creare un semplice form con un campo di input per il titolo di un articolo del blog. Al submit del form, mostrare la lista degli articoli inseriti, con la possibilità di cancellare ciascun articolo utilizzando un'icona.
+- BONUS
+1. Implementare la funzionalità di modifica del titolo di un post
+2. Aggiungere più campi al form (stato dell'articolo(draft, published), autore, ...) 
---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash } from "@fortawesome/free-solid-svg-icons/faTrash";
+import { faTrashAlt } from "@fortawesome/free-regular-svg-icons";
+
+const articles = [
+  { id: 1, title: "Firenze: il gatto della vecchietta conquista il cuore di tutti al mercato" },
+  { id: 2, title: "Bologna: il panino più lungo del mondo sfornato in una trattoria" },
+  { id: 3, title: "Venezia: il gondoliere canta per l'orso polare in visita al Carnevale" }
+];
+
+function App() {
+
+  return (
+    <div className="container">
+      <h1 className="my-5">Aggiungi un articolo</h1>
+      <form action="#">
+        <input type="text" placeholder="Scrivi il titolo dell'articolo" />
+        <button type="submit" className="btn btn-success mx-2">Aggiungi</button>
+      </form>
+
+      <ul className="list-group my-3">
+        {articles.map(art => (
+          <li
+            key={art.id}
+            className="list-group-item d-flex justify-content-between">
+            {art.title}
+            <i>
+              <FontAwesomeIcon icon={faTrashAlt} color="grey" />
+            </i>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
